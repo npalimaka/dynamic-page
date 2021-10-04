1d95d901cd1a9976586d7be5ecf4a52086095751
Natalia Palimaka <npalimaka@gmail.com>
2021-10-04 12:18:39 +0200

    Refactor and readme update

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # dynamic-page
 Transforming static HTML page to dynamic with JavaScript.
+
+## Content
+Project has a simple structure made of HTML file with CSS and Javascript files linked.
+
+```
+css
+- styles.css
+js
+- app.js
+index.html
+```
+## Project goal
+The main goal was to manipulate the DOM by adding navigation pragmatically.
+
+Actions that were taken:
+- Retrieving elements and changing their attributes `querySelector`, `getElementById`, `getElementsByTagName`, `setAttribute`, `style`
+- Creating new elements and adding them `createElement`, `createDocumentFragment`, `appendChild`
+- Adding EventListeners on click and scroll `addEventListener`
+- Using scroll for navigation `scrollIntoView`
+- Applying active state based on viewport `getBoundingClientRect`, `classList`
--- js/app.js
+++ js/app.js
@@ -17,6 +17,7 @@
  * Define Global Variables
  * 
 */
+const sectionsLinks = document.createDocumentFragment();
 const navbarList = document.querySelector('#navbar__list');
 const sectionsHdList = document.getElementsByTagName('h2');
 const sections = document.getElementsByTagName('section');
@@ -26,34 +27,78 @@ const sections = document.getElementsByTagName('section');
  * Start Helper Functions
  * 
 */
-function createLink(section) {
+
+/**
+ * @description Hides navigation bar
+ */
+function hideNav(){
+    document.getElementById('navbar__list').style.display = 'none';
+}
+
+/**
+ * @description Shows navigation bar
+ */
+function showNav(){
+    document.getElementById('navbar__list').style.display = 'block';
+}
+
+/**
+ * @description Adds additional section to main tag
+ */
+function addNewSection() {
+    const firstSection = document.getElementById('section1');
+    const newSection = document.createElement('section');
+    newSection.id = 'section4';
+    newSection.setAttribute('data-nav', 'Section 4')
+    newSection.innerHTML = firstSection.innerHTML;
+
+    const sectionDiv = newSection.firstElementChild;
+    sectionDiv.firstElementChild.innerText = 'Section 4';
+
+    const main = document.querySelector('main');
+    main.appendChild(newSection);
+}
+
+/**
+ * @description Adds section link to navbar
+ * @param {HTMLHeadingElement} sectionName - the name of the section to be linked
+ */
+function createLink(sectionName) {
     const navbarElement = document.createElement('li');
-    const text = document.createTextNode(section.innerText);
+    const text = document.createTextNode(sectionName.innerText);
     navbarElement.appendChild(text);
     navbarElement.setAttribute('class', 'menu__link');
 
-    navbarElement.addEventListener('click', function(){ scrollToSection(section)});
+    navbarElement.addEventListener('click', () => scrollToSection(sectionName));
 
-    navbarList.appendChild(navbarElement);
+    sectionsLinks.appendChild(navbarElement);
 }
 
+/**
+ * @description Scrolls to provided element on page
+ * @param element
+ */
 function scrollToSection(element) {
     element.scrollIntoView({ behavior: 'smooth', block: 'center' });
 }
-
 
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
-// build the nav
+// Adding 4th section
+addNewSection();
+
+// Building the nav
 for (const section of sectionsHdList) {
     createLink(section);
 }
+navbarList.appendChild(sectionsLinks);
 
-// Add class 'active' to section when near top of viewport
-document.addEventListener('scroll', function (){
+// Adding class 'active' to section when near top of viewport
+document.addEventListener('scroll', () => {
+    showNav();
     for (const section of sections) {
         if (section.getBoundingClientRect().top <= window.innerHeight / 2
             && section.getBoundingClientRect().top + section.getBoundingClientRect().height > window.innerHeight / 2) {
@@ -62,19 +107,5 @@ document.addEventListener('scroll', function (){
             section.classList.remove('active');
         }
     }
+    setTimeout(hideNav, 3000);
 })
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
