bb554780828d82f7197d862395277423c533d8fd
Natalia Palimaka <npalimaka@gmail.com>
2021-10-04 09:30:38 +0200

    Navigation added

--- js/app.js
+++ js/app.js
@@ -17,14 +17,29 @@
  * Define Global Variables
  * 
 */
-
+const navbarList = document.querySelector('#navbar__list');
+const sectionsHdList = document.getElementsByTagName('h2');
+const sections = document.getElementsByTagName('section');
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+function createLink(section) {
+    const navbarElement = document.createElement('li');
+    const text = document.createTextNode(section.innerText);
+    navbarElement.appendChild(text);
+    navbarElement.setAttribute('class', 'menu__link');
 
+    navbarElement.addEventListener('click', function(){ scrollToSection(section)});
+
+    navbarList.appendChild(navbarElement);
+}
+
+function scrollToSection(element) {
+    element.scrollIntoView({ behavior: 'smooth', block: 'center' });
+}
 
 
 /**
@@ -32,14 +47,22 @@
  * Begin Main Functions
  * 
 */
-
 // build the nav
-
+for (const section of sectionsHdList) {
+    createLink(section);
+}
 
 // Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
+document.addEventListener('scroll', function (){
+    for (const section of sections) {
+        if (section.getBoundingClientRect().top <= window.innerHeight / 2
+            && section.getBoundingClientRect().top + section.getBoundingClientRect().height > window.innerHeight / 2) {
+            section.classList.add('active');
+        } else {
+            section.classList.remove('active');
+        }
+    }
+})
 
 
 /**
